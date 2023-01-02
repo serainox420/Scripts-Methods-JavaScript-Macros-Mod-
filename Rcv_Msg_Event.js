290295fee516f2da031c8cd2ea23368181eb91d4
Silverainox.OS <serainox@gmail.com>
2023-01-02 09:56:03 +0100

    Create Rcv_Msg_Event.js

---
+++ Rcv_Msg_Event.js
@@ -0,0 +1,18 @@
+// Literally simplest Rcv MSG event example
+
+if (event.text.getString() == "Message")
+    
+// JsMacros.runScript("Script.js")
+// Chat.say("Response")
+  
+  // Example output 
+  // (Single reactive line for each event listener)
+  
+ // Cheap way how to detect selected player messages
+  
+  if (event.text.getString() == "<Nickname> Message")
+Chat.say("Reply")
+
+// Note, it is way better to use arrays, functions and variables for Remote Control Script based on RCV MSG
+
+  
