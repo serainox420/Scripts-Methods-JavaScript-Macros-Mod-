7d319393b73b9a3f281f8d77f161344ce19cc4a1
Silverainox.OS <serainox@gmail.com>
2023-01-02 09:41:37 +0100

    Create Entity_Event.js

---
+++ Entity_Event.js
@@ -0,0 +1,16 @@
+//Detect Entities & React
+//WORKING ONLY SOMEWHAT
+
+//World.getPlayers();
+
+//Chat.log(World.getLoadedPlayers());
+Chat.log(event.entity.getType());
+if (event.entity.getType() == "minecraft:creeper")
+
+if (event.entity.getType() == "minecraft:creeper")
+Chat.say("CREEPER ALERT")
+
+///Chat.log(event.entity.getName());
+///if (event.entity.getName() == "serainox")
+///Chat.say("DETECTED")
+  
